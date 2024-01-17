3fd36000ce12d339cd80d29f6fee27e7212edb5d
sandykr29 <sandeepkumar29774@yahoo.com>
2024-01-17 23:40:48 +0530

    about initiated

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frontend part...</h1>
+    </div>
+  );
+}
+
+export default App;

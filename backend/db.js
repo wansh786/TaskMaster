f86e42227f03f8b4baeb5ac4ec21dcad3c4afac7
Raghuwansh Kumar Singh <121174683+wansh786@users.noreply.github.com>
2024-01-22 13:38:27 +0530

    Delete backend directory

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-
-// require("dotenv").config();
-
-const connection=mongoose.connect("mongodb+srv://raghuwansh:singh@cluster0.ux37wqw.mongodb.net/TaskMaster?retryWrites=true&w=majority")
-
-module.exports={
-    connection
-}
